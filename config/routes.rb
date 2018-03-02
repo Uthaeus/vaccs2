83e85cd511751899ca00ba705c826153c9be263e
Uthaeus <romanlavery@gmail.com>
2018-03-02 15:36:40 -0700

    Installed devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  
+  devise_for :users
+  root to: 'pages#home'
 
   get 'pages/client'
 
   resources :events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
